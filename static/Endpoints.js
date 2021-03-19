3944457a0aacbd23f4cf2b6bf44d937c85976962
Broscientist <victorsun1685@gmail.com>
2021-03-18 23:05:52 -0700

    change Endpoint.js for redeployment

--- static/Endpoints.js
+++ static/Endpoints.js
@@ -1,23 +1,23 @@
 //////////////////////////// DEPLOYMENT ///////////////////////////////////
-// var baseURL = "https://snippet-stack.herokuapp.com"
-// var urlForCreate = "https://snippet-stack.herokuapp.com/api/create_snippet";
-// var urlForDelete = "https://snippet-stack.herokuapp.com/api/delete_snippet";
-// var urlForSearch = "https://snippet-stack.herokuapp.com/api/fetch_snippet";
-// var urlForLikes = "https://snippet-stack.herokuapp.com/api/like_snippet";
-// var urlForEditSnippet = "https://snippet-stack.herokuapp.com/api/edit_snippet";
-// var urlForGettingComments = "https://snippet-stack.herokuapp.com/api/get_comments_for_snippet";
-// var urlForComments = "https://snippet-stack.herokuapp.com/api/comment_snippet";
-// var urlForRequest = "https://snippet-stack.herokuapp.com/api/request_snippet";
-// var urlForEditComment = "https://snippet-stack.herokuapp.com/api/edit_comment";
+var baseURL = "https://snippet-stack.herokuapp.com"
+var urlForCreate = "https://snippet-stack.herokuapp.com/api/create_snippet";
+var urlForDelete = "https://snippet-stack.herokuapp.com/api/delete_snippet";
+var urlForSearch = "https://snippet-stack.herokuapp.com/api/fetch_snippet";
+var urlForLikes = "https://snippet-stack.herokuapp.com/api/like_snippet";
+var urlForEditSnippet = "https://snippet-stack.herokuapp.com/api/edit_snippet";
+var urlForGettingComments = "https://snippet-stack.herokuapp.com/api/get_comments_for_snippet";
+var urlForComments = "https://snippet-stack.herokuapp.com/api/comment_snippet";
+var urlForRequest = "https://snippet-stack.herokuapp.com/api/request_snippet";
+var urlForEditComment = "https://snippet-stack.herokuapp.com/api/edit_comment";
 
 //////////////////////////// DEBUGGING ///////////////////////////////////
-var baseURL = "http://127.0.0.1:5000";
-var urlForCreate = 'http://127.0.0.1:5000/api/create_snippet';
-var urlForDelete = "http://127.0.0.1:5000/api/delete_snippet";
-var urlForSearch = "http://127.0.0.1:5000/api/fetch_snippet";
-var urlForLikes = "http://127.0.0.1:5000/api/like_snippet";
-var urlForEditSnippet = "http://127.0.0.1:5000/api/edit_snippet";
-var urlForGettingComments = "http://127.0.0.1:5000/api/get_comments_for_snippet";
-var urlForComments = "http://127.0.0.1:5000/api/comment_snippet";
-var urlForRequest = "http://127.0.0.1:5000/api/request_snippet";
-var urlForEditComment = "http://127.0.0.1:5000/api/edit_comment";
+// var baseURL = "http://127.0.0.1:5000";
+// var urlForCreate = 'http://127.0.0.1:5000/api/create_snippet';
+// var urlForDelete = "http://127.0.0.1:5000/api/delete_snippet";
+// var urlForSearch = "http://127.0.0.1:5000/api/fetch_snippet";
+// var urlForLikes = "http://127.0.0.1:5000/api/like_snippet";
+// var urlForEditSnippet = "http://127.0.0.1:5000/api/edit_snippet";
+// var urlForGettingComments = "http://127.0.0.1:5000/api/get_comments_for_snippet";
+// var urlForComments = "http://127.0.0.1:5000/api/comment_snippet";
+// var urlForRequest = "http://127.0.0.1:5000/api/request_snippet";
+// var urlForEditComment = "http://127.0.0.1:5000/api/edit_comment";
